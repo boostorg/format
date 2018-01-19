587c66bf924e92865379442b2577a6e9379f9a91
James E. King, III <jking@apache.org>
2018-01-19 12:53:14 -0500

    Update CI adding cppcheck support and tweak UBSAN settings

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Copyright 2017 James E. King, III
+# Copyright 2017 James E. King III
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 #      http://www.boost.org/LICENSE_1_0.txt)
@@ -9,11 +9,11 @@
 # cwd should be $BOOST_ROOT/libs/$SELF before running
 #
 
+set -ex
+
 # default language level: c++03
 if [[ -z "$CXXSTD" ]]; then
     CXXSTD=03
 fi
 
-echo "$BOOST_ROOT/b2 . toolset=$TOOLSET cxxstd=$CXXSTD $CXXFLAGS $LINKFLAGS $TESTFLAGS $B2_ADDRESS_MODEL $B2_LINK $B2_THREADING $B2_VARIANT -j3 $*"
-      $BOOST_ROOT/b2 . toolset=$TOOLSET cxxstd=$CXXSTD $CXXFLAGS $LINKFLAGS $TESTFLAGS $B2_ADDRESS_MODEL $B2_LINK $B2_THREADING $B2_VARIANT -j3 $*
-
+$BOOST_ROOT/b2 . toolset=$TOOLSET cxxstd=$CXXSTD $CXXFLAGS $DEFINES $LINKFLAGS $TESTFLAGS $B2_ADDRESS_MODEL $B2_LINK $B2_THREADING $B2_VARIANT -j3 $*
