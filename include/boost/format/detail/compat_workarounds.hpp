9c94af767197f03ee8118822b6b9e23e092ce853
Georgiy Guminov <gogagum@gmail.com>
2024-06-07 19:36:18 +0300

    Remove workaround for gcc 2_95.

--- include/boost/format/detail/compat_workarounds.hpp
+++ include/boost/format/detail/compat_workarounds.hpp
@@ -30,37 +30,8 @@
 #ifndef BOOST_FORMAT_COMPAT_WORKAROUNDS_HPP
 #define BOOST_FORMAT_COMPAT_WORKAROUNDS_HPP
 
-namespace boost {
-    namespace io {
-
-        // gcc-2.95 char traits (non-conformantly named string_char_traits) 
-        // lack several functions so we extend them in a replacement class.
-        template<class Tr>
-        class CompatTraits; 
-
-        // std::allocator<Ch> in gcc-2.95 is ok, but basic_string only works 
-        // with plain 'std::alloc' still, alt_stringbuf requires a functionnal
-        // alloc template argument, so we need a replacement allocator
-        template<class Alloc>
-        class CompatAlloc; 
-    } // N.S. io
-}// N.S. boost
-
-
 #include <boost/format/detail/config_macros.hpp>
    // sets-up macros and load compiler-specific workarounds headers.
-
-#if !defined(BOOST_FORMAT_STREAMBUF_DEFINED)
-// workarounds-gcc-2.95 might have defined own streambuf
-#include <streambuf>
-#endif
-
-#if !defined(BOOST_FORMAT_OSTREAM_DEFINED)
-// workarounds-gcc-2.95 might already have included <iostream>
-#include <ostream>
-#endif
-
-
 
 namespace boost {
     namespace io {
