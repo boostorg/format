a670f112e61a99a67bd8841efa1bf463db034706
Alexander Grund <Flamefire@users.noreply.github.com>
2026-06-23 10:24:17 +0200

    Use conditional noexceptness

--- include/boost/format/format_implementation.hpp
+++ include/boost/format/format_implementation.hpp
@@ -82,7 +82,7 @@ namespace boost {
     }
     template< class Ch, class Tr, class Alloc>
     void  basic_format<Ch, Tr, Alloc>:: 
-    swap (basic_format & x) noexcept {
+    swap (basic_format & x) BOOST_NOEXCEPT_IF(BOOST_NOEXCEPT_EXPR(this->items_.swap(x.items_))) {
         std::swap(exceptions_, x.exceptions_);
         std::swap(style_, x.style_); 
         std::swap(cur_arg_, x.cur_arg_); 
